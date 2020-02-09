f3b320bf768892663c8539c333bb486fb2cdea1a
Sniizy <56795610+Sniizy@users.noreply.github.com>
2020-02-09 10:29:34 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const fs = require('fs');
 
-client.login("Njc1OTE3Mjc5NjQwNDIwMzY0.Xj_O8w.xpcbdcGwRN5ixHo0Kj6cvi6TosA");
+client.login("process.env.TOKEN");
 
 
 fs.readdir('./Commandes/', (error, f) => {
